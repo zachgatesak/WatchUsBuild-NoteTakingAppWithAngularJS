e8467d210e4d2ef30e7630d84e91824c9da49335
Gates <zachgatesak@gmail.com>
2017-02-19 17:33:14 -0600

    Add toggle function to category list

--- app/assets/javascript/directives/nwCategorySelector.js
+++ app/assets/javascript/directives/nwCategorySelector.js
@@ -4,7 +4,19 @@ angular.module('NoteWrangler').directive("nwCategorySelector", function(Category
     restrict: 'E',
     templateUrl: '/assets/templates/directives/nwCategorySelector.html',
     link: function(scope, element, attrs){
+        var activeCategory = {};
         scope.categories = Category.query()
+        scope.isActive = function(category){
+          return activeCategory.id === category.id;
+        }
+        scope.toggleCategory = function(category){
+          if(category === activeCategory){
+            activeCategory = {};
+          }
+          else {
+          activeCategory = category;
+          }
+      }
     }
   };
 
