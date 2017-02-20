4493d9522b0fe2d685406bd1ce4aa4afaca6a57a
Gates <zachgatesak@gmail.com>
2017-02-19 22:01:29 -0600

    Add categories and User selection to new note section.

--- app/assets/javascript/controllers/notesCreateController.js
+++ app/assets/javascript/controllers/notesCreateController.js
@@ -1,6 +1,8 @@
-angular.module('NoteWrangler').controller('NotesCreateController', function($scope, Note, $location){
+angular.module('NoteWrangler').controller('NotesCreateController', function($scope, Note, Category, User, $location){
   $scope.note = new Note();
   $scope.isSubmitting = false;
+  $scope.categories = Category.query();
+  $scope.users = User.query();
 
   $scope.saveNote = function(note){
     $scope.isSubmitting = true;
