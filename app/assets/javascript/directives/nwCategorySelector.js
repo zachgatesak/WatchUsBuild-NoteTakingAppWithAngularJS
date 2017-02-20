0a69d9db0ba135449c112024e5795f634b544d96
Gates <zachgatesak@gmail.com>
2017-02-19 21:19:49 -0600

    add filtering based on selected tag

--- app/assets/javascript/directives/nwCategorySelector.js
+++ app/assets/javascript/directives/nwCategorySelector.js
@@ -2,12 +2,13 @@ angular.module('NoteWrangler').directive("nwCategorySelector", function(Category
   return {
     replace: true,
     restrict: 'E',
+    require: '?ngModel',
     templateUrl: '/assets/templates/directives/nwCategorySelector.html',
-    link: function(scope, element, attrs){
+    link: function(scope, element, attrs, ngModelCtrl){
         var activeCategory = {};
         scope.categories = Category.query()
         scope.isActive = function(category){
-          return activeCategory.id === category.id;
+          return activeCategory && activeCategory.id === category.id;
         }
         scope.toggleCategory = function(category){
           if(category === activeCategory){
@@ -16,6 +17,10 @@ angular.module('NoteWrangler').directive("nwCategorySelector", function(Category
           else {
           activeCategory = category;
           }
+          ngModelCtrl.$setViewValue(activeCategory);
+      }
+      ngModelCtrl.$render = function(){
+        activeCategory = ngModelCtrl.$viewValue;
       }
     }
   };
