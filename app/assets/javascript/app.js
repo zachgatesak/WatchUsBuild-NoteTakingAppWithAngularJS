f6b2e1893f058bc788deaf007f5ada5ee2df7d9b
Gates <zachgatesak@gmail.com>
2017-02-20 08:31:29 -0600

    add googlecode md5 and gravatar fetch to users

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -1,1 +1,3 @@
-angular.module('NoteWrangler', ['ngRoute','ngResource']);
+angular.module('NoteWrangler', ['ngRoute','ngResource','ngGravatar']).config(function(GravatarProvider){
+  GravatarProvider.setSize(100);
+});
---
+++ app/assets/javascript/directives/title.js
@@ -0,0 +1,10 @@
+angular.module('NoteWrangler').directive('title', function($timeout){
+  return function(scope, element, attrs){
+    $timeout(function(){
+      $(element).tooltip();
+    });
+    scope.$on('$destroy',function(){
+      $(element).tooltip();
+    });
+  };
+});
